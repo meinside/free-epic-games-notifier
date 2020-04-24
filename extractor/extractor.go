adc35752eb0f65b864e844dae6576d8e4e668bb2
Sungjin Han <meinside@gmail.com>
2020-04-24 12:31:04 +0900

    Fix to wait for titles

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -38,7 +38,7 @@ func ExtractFreeGames() ([]FreeGame, error) {
 	var imgs []*cdp.Node
 	err := chromedp.Run(runCtx,
 		chromedp.Navigate(storeURL),
-		chromedp.WaitVisible(`div[class^=OfferCardImage]`),
+		chromedp.WaitVisible(`span[class^=OfferTitleInfo-title]`),
 		chromedp.Nodes(`span[class^=OfferTitleInfo-title]`, &titles, chromedp.ByQueryAll),
 		chromedp.Nodes(`span[class^=AvailabilityStatusBar-root]`, &statuses, chromedp.ByQueryAll),
 		chromedp.Nodes(`div[class^=CardGrid-card] > a`, &links, chromedp.ByQueryAll),
